28bb4d74283e76724dc7a8b029bdabb93bf6a54f
CaoDuy132 <docaoduy132@gmail.com>
2023-05-09 17:52:37 +0700

    add logic reset game

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ import { getCellElementList,
     getCellListElement
  } from "./selectors.js";
  import {checkGameStatus} from './utils.js';
- console.log(checkGameStatus(['X', 'O', 'O', '', 'X', '', '', 'O', 'X']));
 /**
  * Global variables
  */
 let currentTurn = TURN.CROSS;
-let arrChecked=[];
+let gameStatus = GAME_STATUS.PLAYING;
+let cellValues = new Array(9).fill("");
 const toggleTurn = ()=>{
     if(currentTurn == TURN.CROSS){
         currentTurn = TURN.CIRCLE;
@@ -22,21 +22,79 @@ const toggleTurn = ()=>{
     getCurrentTurnElement().classList.remove(TURN.CROSS,TURN.CIRCLE)
     getCurrentTurnElement().classList.add(currentTurn)
 }
+function updateGameStatus(newGameStatus) {
+    gameStatus = newGameStatus;
+  
+    const gameStatusElement = getGameStatusElement();
+    if (gameStatusElement) gameStatusElement.textContent = newGameStatus;
+  }
+  
+  function showReplayButton() {
+    const replayButton = getReplayButtonElement();
+    if (replayButton) replayButton.classList.add("show");
+  }
+  
+  function hideReplayButton() {
+    const replayButton = getReplayButtonElement();
+    if (replayButton) replayButton.classList.remove("show");
+  }
+  
+  function highlightWinCells(winPositions) {
+    if (!Array.isArray(winPositions) || winPositions.length !== 3) {
+      throw new Error("Invalid win positions");
+    }
+  
+    for (const position of winPositions) {
+      const cell = getCellElementAtIdx(position);
+      console.log(cell);
+      if (cell) cell.classList.add("win");
+    }
+  }
+  
 const handleClickCell = (cellElement,index)=>{
     const isChecked = cellElement.classList.contains(TURN.CROSS)||cellElement.classList.contains(TURN.CIRCLE);
     if(isChecked) return ;
     cellElement.classList.add(getCurrentTurnElement().getAttribute('class'));
+    // update cellValues
+    cellValues[index] =
+    currentTurn === TURN.CIRCLE ? CELL_VALUE.CIRCLE : CELL_VALUE.CROSS;
     toggleTurn();
+      // check game status
+  const game = checkGameStatus(cellValues);
+  switch (game.status) {
+    case GAME_STATUS.ENDED: {
+      updateGameStatus(game.status);
+      showReplayButton();
+      break;
+    }
+
+    case GAME_STATUS.X_WIN:
+    case GAME_STATUS.O_WIN: {
+      updateGameStatus(game.status);
+      showReplayButton();
+      highlightWinCells(game.winPositions);
+      break;
+    }
+    default:
+    // playing
+  }
 }
 const CellElementList =  getCellElementList();
+const btnReplay = document.getElementById("replayGame");
+btnReplay.addEventListener('click',()=>{
+    CellElementList.forEach(item=>{
+        cellValues = cellValues.map(() => "");
+        item.className = "";
+        updateGameStatus(GAME_STATUS.PLAYING);
+        hideReplayButton();
+    })
+})
 CellElementList.forEach((cellElement,index)=>{
     cellElement.addEventListener('click',()=>{
         handleClickCell(cellElement,index);
     })
 })
-let isGameEnded = false;
-let cellValues = new Array(9).fill("");
-const click = document.getElementById('cellList')
+
 /**
  * TODOs
  *
